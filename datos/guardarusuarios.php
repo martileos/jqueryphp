d5268b0a687a5762414c7defbd824cd0803e5b8e
Martin Nevarez <martinevarez@gmail.com>
2015-05-25 10:35:49 -0600

    Agregar un select a la captura de usuarios

--- datos/guardarusuarios.php
+++ datos/guardarusuarios.php
@@ -4,11 +4,11 @@ function guardarusuarios()
 	$respuesta = false;
 	$usuario   = "'".$_POST["usuario"]."'";
 	$nombre    = "'".$_POST["nombre"]."'";
-	$tipo      = $_POST["tipo"];
+	$tipo      = "'".$_POST["tipo"]."'";
 	$con=mysql_connect("localhost","root","");
 	mysql_select_db("pw10am");
 	// $consulta = "insert into usuarios(usuario,nombre,tipousuario) values(".$usuario.",".$nombre.",".$tipo.")";
-	$consulta = sprintf("insert into usuarios(usuario,nombre,tipousuario)values(%s,%s,%d)",$usuario,$nombre,$tipo);
+	$consulta = sprintf("insert into usuarios(usuario,nombre,tipousuario)values(%s,%s,%s)",$usuario,$nombre,$tipo);
 	mysql_query($consulta);
 	if(mysql_affected_rows()>0)
 	{
